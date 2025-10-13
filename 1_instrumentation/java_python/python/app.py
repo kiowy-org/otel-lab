c109ac29ba6278ec453a44f61d8bfa182ba3603f
Benjamin ROZIERE <broziere@veepee.com>
2025-10-13 17:02:40 +0200

    Reponses java python auto instrumentation

--- 1_instrumentation/java_python/python/app.py
+++ 1_instrumentation/java_python/python/app.py
@@ -1,10 +1,14 @@
+import logging
 from flask import Flask, request, jsonify
+
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     print(request.args.get("param"))
+    logging.info("Hello, World!")
     return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
